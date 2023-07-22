4113439f24414d67f459b97a4cdeebe99fdc6d03
Maren Keller <104692152+marenkeller@users.noreply.github.com>
2023-07-22 16:03:04 +0200

    Initial commit/ Setup

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from "./babycats.jpeg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="btn btn-primary"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Bootstrap
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
