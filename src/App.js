2af96d95739365b9a3b3c336fe4220725561a9ac
Maren Keller <104692152+marenkeller@users.noreply.github.com>
2023-07-24 18:18:23 +0200

    Adding Search Engine component (Dictionary)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from "./babycats.jpeg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="btn btn-primary"
-          href="#https://getbootstrap.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Bootstrap
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <p>World of Words: Your Online Dictionary</p>
+          <a
+            className="btn btn-primary"
+            href="#https://getbootstrap.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Bootstrap
+          </a>
+        </header>
+
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer text-center"> Coded by Maren</footer>
+      </div>
     </div>
   );
 }
